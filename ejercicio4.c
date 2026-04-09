0aae4eed485e2313a33ea94b6529a2cf7c995b36
zum-15 <bautistazunny@gmail.com>
2026-04-09 11:53:34 -0600

    Add files via upload

---
+++ ejercicio4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+	float total, desceunto=0, totalfinal;
+	printf("total de compra: ");
+	scanf("5f", &total); 
+	if(total > 1000)
+                 descuento = total * 0.10;
+	totalfinal = total - descuento;
+	printf("total final: %.2f", totalfinal);
+	return 0;
+}
+ #include
+ int main(){
+ char prod
