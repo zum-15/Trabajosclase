2f05b89d6506583916ede57a528376baa0b9a6ab
zum-15 <bautistazunny@gmail.com>
2026-04-09 12:07:49 -0600

    Add files via upload

---
+++ desiciones 1.6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+float dinero; int abierta;
+scanf("%f",&dinero);
+if(dinero>=50){
+scanf("%d",&abierta);
+if(abierta==1){
+printf("Puedes comprar\n");
+}
+}
+return 0;
+}
